f4b3e63a454125e993dacf21b04684e3b66569dc
Seshathri <seshathri2008@gmail.com>
2024-03-18 19:23:36 +0530

    inital commit

---
+++ src/views/Maps.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+// react-bootstrap components
+import { Badge, Button, Navbar, Nav, Container } from "react-bootstrap";
+
+
+
+function Maps() {
+  const mapRef = React.useRef(null);
+  React.useEffect(() => {
+    let google = window.google;
+    let map = mapRef.current;
+    let lat = "13.067439";
+    let lng = "80.237617";
+    const myLatlng = new google.maps.LatLng(lat, lng);
+    const mapOptions = {
+      zoom: 13,
+      center: myLatlng,
+      scrollwheel: false,
+      zoomControl: true,
+    };
+
+    map = new google.maps.Map(map, mapOptions);
+
+    const marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      animation: google.maps.Animation.DROP,
+      title: "Need to Show the Data Here",
+    });
+
+    const contentString =
+      '<div class="info-window-content"><h2>Need To show Data Here</h2>' +
+      "<p>Location : Chennai </p></div>";
+
+    const infowindow = new google.maps.InfoWindow({
+      content: contentString,
+    });
+
+    google.maps.event.addListener(marker, "click", function () {
+      infowindow.open(map, marker);
+    });
+  }, []);
+  return (
+    <>
+      <div className="map-container">
+        <div id="map" ref={mapRef}></div>
+      </div>
+    </>
+  );
+}
+
+export default Maps;
+
+
+
